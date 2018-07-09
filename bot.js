0893dfbf033cdf07b4becc3d566c3dbb2d02b7cc
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-10 01:59:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,101 @@ client.user.setGame(`Playing`,"#help")
     console.log(` Join Bot Of Server ${guild.name} Owner Of Server ${guild.owner.user.username}!`)
   });
   
+//warn 
+client.on('message', msg => { 
+    if (msg.content.startsWith('*warn')) {
+      if(!msg.member.hasPermission("MUTE_MEMBERS")) return;
+       let args = msg.content.split(" ").slice(1);
+      if (!msg.mentions.members.first()) return msg.reply('**mention a user/player ```Exp: *warn @unknown1547 spamming```**')
+      if (!args[1]) return msg.reply('**Reason for warning**')
+      if (msg.guild.channels.find('name', '⚠-warns')) {
+        msg.guild.channels.find('name', '⚠-warns').send(`
+      ***You have been warned*** : ${msg.mentions.members.first()}
+      ***___Because you did the following___***
+      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
+      `)
+      }
+    }
+});
+
+//ban
+client.on("message", function(message) {
+    let toBan = message.mentions.users.first();
+    let toReason = message.content.split(" ").slice(2).join(" ");
+    let toEmbed = new Discord.RichEmbed()
+    var prefix = "*"
+   if(message.content.startsWith(prefix + "ban")) {
+       if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("**# - You dont have enough permissions!**");
+       if(!toBan) return message.reply("**# - Mention a user! ```Exp: *ban @unknown1547 spamming```**");
+       if(toBan.id === ("447121312960479242")) return message.reply("**# You cannot ban me!**");
+       if(toBan === message.member.guild.owner) return message.reply("**# You cannot ban the owner of the server!**");
+       if(toBan.bannable) return message.reply("**# - I cannot ban someone with a higher role than me!**");
+       if(!toReason) return message.reply("**# - Supply a reason!**")
+       if(toBan.id === message.author.id) return message.reply("**# You cannot ban yourself!**")
+       if(!message.guild.member(toBan).bannable) return message.reply("**# - I cannot ban this person!**")
+       let toEmbed;
+       toEmbed = new Discord.RichEmbed()
+       .setTitle("You have been banned from a server!")
+       .setThumbnail(toBan.avatarURL)
+       .addField("**# - Server:**",message.guild.name,true)
+       .addField("**# - Reason:**",toReason,true)
+       .addField("**# - Banned By:**",message.author,true)
+       if(message.member.hasPermission("BAN_MEMBERS")) return (
+           toBan.sendMessage({embed: toEmbed}).then(() => message.guild.member(toBan).ban({reason: toReason})).then(() => message.channel.send(`**# Done! I banned: ${toBan}**`))
+       );
+       
+   }
+});
+
+//kick
+client.on('message',function(message) {
+    let toKick = message.mentions.users.first();
+    let toReason = message.content.split(" ").slice(2).join(" ");
+    let toEmbed = new Discord.RichEmbed()
+    var prefix = "*"
+   if(message.content.startsWith(prefix + 'kick')) {
+       if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply('**# - You dont have enough permissions!**');
+       if(!toReason) return message.reply("**# - Mention a user!  ```Exp: *kick @unknown1547 spamming```**")
+       if(toKick.id === message.author.id) return message.reply("**# You cannot kick yourself!**")
+       if(!message.guild.member(toKick).bannable) return message.reply("**# - I cannot ban this person!**")
+       let toEmbed;
+       toEmbed = new Discord.RichEmbed()
+       .setTitle("You have been kicked from a server!")
+       .setThumbnail(toKick.avatarURL)
+       .addField("**# - Server:**",message.guild.name,true)
+       .addField("**# - Reason:**",toReason,true)
+       .addField("**# - Kicked By:**",message.author,true)
+       if(message.member.hasPermission("KICK_MEMBERS")) return (
+           toKick.sendMessage({embed: toEmbed}).then(() => message.guild.member(toKick).kick()).then(() => message.channel.send(`**# Done! I kicked: ${toKick}**`))
+       )
+       }
+});
+
+//clear
+client.on('message', msg => {
+  var prefix ="*"
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```Set the number of messages you want to delete 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\n The number of messages that have been cleared: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
   
   
   client.login(process.env.BOT_TOKEN);
