18ef1702ffd09c58001e592908a8dbf4b46ca75f
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-11 02:31:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -935,6 +935,54 @@ if(!args[0]) return message.reply('**Please write the text you want**');
            })
 }
 });
+//translate 
+const translate = require('google-translate-api');
+const Langs = ['afrikaans','albanian','amharic','arabic','armenian','azerbaijani','bangla','basque','belarusian','bengali','bosnian','bulgarian','burmese','catalan','cebuano','chichewa','chinese simplified','chinese traditional','corsican','croatian','czech','danish','dutch','english','esperanto','estonian','filipino','finnish','french','frisian','galician','georgian','german','greek','gujarati','haitian creole','hausa','hawaiian','hebrew','hindi','hmong','hungarian','icelandic','igbo','indonesian','irish','italian','japanese','javanese','kannada','kazakh','khmer','korean','kurdish (kurmanji)','kyrgyz','lao','latin','latvian','lithuanian','luxembourgish','macedonian','malagasy','malay','malayalam','maltese','maori','marathi','mongolian','myanmar (burmese)','nepali','norwegian','nyanja','pashto','persian','polish','portugese','punjabi','romanian','russian','samoan','scottish gaelic','serbian','sesotho','shona','sindhi','sinhala','slovak','slovenian','somali','spanish','sundanese','swahili','swedish','tajik','tamil','telugu','thai','turkish','ukrainian','urdu','uzbek','vietnamese','welsh','xhosa','yiddish','yoruba','zulu'];
+
+client.on('message' , async (message) => {
+  var prefix = "*"
+       if(message.content.startsWith(prefix + "translate")) {
+              let args = message.content.split(" ").slice(1);
+
+  if (args[0] === undefined) {
+
+    const embed = new Discord.RichEmbed()
+    .setColor("FFFFFF")
+    .setDescription("**Provide a __language__ and some __text__ for bot to translate.**\n```Example:  *translate japanese Hello```");
+
+    return message.channel.send(embed);
+
+  } else {
+
+    if (args[1] === undefined) {
+
+      return message.channel.send('**Please write __something__ to translate.** ```Example:  *translate japanese Hello```');
+
+    } else {
+
+      let transArg = args[0].toLowerCase();
+       args = args.join(' ').slice(prefix.length);
+      let translation;
+
+      if (!Langs.includes(transArg)) return message.channel.send(`**Language not found.**`);
+      args = args.slice(transArg.length);
+
+      translate(args, {to: transArg}).then(res => {
+
+        const embed = new Discord.RichEmbed()
+        .setDescription(res.text)
+        .setFooter(`english -> ${transArg}`)
+        .setColor(`RANDOM`);
+        return message.channel.send(embed);
+
+      });
+
+    }
+
+  }
+
+}
+});
 ///Help Codes
 client.on('message', message => {
     if (message.content === "*help-2") {
