ae67e6a23fb42f6e7503b7628e92259ce6903546
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-11 00:10:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -465,5 +465,35 @@ let embed = new Discord.RichEmbed()
 message.channel.sendEmbed(embed);
 }
 });
+//achieve
+const sql = require("sqlite");
+client.on("message", async message => {
+  var prefix = "*"
+    if (message.content.startsWith(prefix + "achieve")) {
+         var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("Put something you want to achieve!");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+    }
+});
  
   client.login(process.env.BOT_TOKEN);
