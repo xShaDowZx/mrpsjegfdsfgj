9d84af1f62ca8a67a3999fd19f21f463b8c9cb0d
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-11 03:19:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Playing`,"#help")
+client.user.setActivity(`Playing`,"#help")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
