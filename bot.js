399ba8b903e1cdfdd271a9f77e75762eb3fad6c7
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-13 14:35:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1100,6 +1100,91 @@ client.on('message', message => {
     return message.reply(`** :anger: __Advertising__ is not allowed here! :angry: **`)
     }
 });
+//Anti Spam
+const ms = require("ms");
+
+const fs = require("fs");
+
+
+var user = {};
+var warn = {};
+
+client.on('message', function(message) {
+
+    	 if (!message.channel.guild) return;
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+
+  if (message.author.id == client.user.id) return;
+  if(JSON.stringify(user).indexOf(message.author.id) == -1) {
+    user[message.author.id] = message.createdTimestamp;
+    return;
+  } else {
+    if (Date.now() - user[message.author.id] < 695){
+              message.author.delete
+
+      if (JSON.stringify(warn).indexOf(message.author.id) == -1) {
+        warn[message.author.id] = 1;
+      } else {
+        warn[message.author.id]++;
+        message.author.delete
+      }
+      if (warn[message.author.id] < 4) {
+        message.author.delete
+
+      }
+      delete user[message.author.id];
+              message.author.delete
+
+    } else {
+      delete user[message.author.id];
+              message.author.delete
+
+    }
+  }
+  if (warn[message.author.id] == 4) {		   
+     if (!message.channel.guild) return;
+             message.author.delete
+
+let muteRole1 = message.guild.roles.find("name", "Muted");
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+          var currentTime = new Date(),
+                   Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate(),
+hours = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes()+1,
+            seconds = currentTime.getSeconds();
+
+           if (!message.channel.guild) return;
+     if (!muteRole1) return;
+    var guild = message.channel.guild;
+    message.guild.members.get(message.author.id).addRole(muteRole1);
+    
+     var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+delete warn[message.author.id];
+    delete user[message.author.id];
+	const embed500 = new Discord.RichEmbed()
+     .setTitle(`Sender ${message.author.username}#${message.author.discriminator} `)
+      .setDescription("**:white_check_mark:  | `Spamming`\n\nName:\n"+`${message.author.username}#${message.author.discriminator}`+"\nYou got\n  __Muted__\n**")
+      .setFooter("【✭ Nameless Bot Anti-Spam ✭】")
+      .setColor("c91616")
+    message.channel.send(embed500)
+    	const embed20 = new Discord.RichEmbed()
+      .setTitle("**:scales: | You have been Muted**")
+      .setDescription(`**:small_blue_diamond: You have break the server rules\n \n:gun: : You got __Muted__\n :clock1: Time and date of punishment:\n`+ Year + "/" + Month + "/" + Day +', '+hours +'-' +minutes+'-'+seconds+"\n \n \n`In case the mute by mistake, contact server staff`**")
+          .setFooter("【✭ Nameless Bot Anti-Spam ✭】")
+      .setColor("c91616")
+    
+     message.author.send(embed20)
+  
+  }
+});
 
 
 
